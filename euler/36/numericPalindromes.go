db8958d03c314ea18c31594aa11429ccb6f760d5
Chris <aczietlow@gmail.com>
2021-06-17 18:39:03 -0400

    Euler project 36

--- euler/36/numericPalindromes.go
+++ euler/36/numericPalindromes.go
@@ -8,7 +8,9 @@ import (
 
 // Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 func main() {
+	// Use big Int to leverage easy String and Text methods. String comp are faster then numeric computations.
 	limit, count, sum := big.NewInt(1000000), big.NewInt(1), big.NewInt(0)
+	// Count is 2, b/c since a base 2 number can't have compare leading 0's, implies base 2 numbers will never end in 0.
 	for i := big.NewInt(1); i.Cmp(limit) == -1; i.Add(i, count) {
 		if isPalindromes(*i) == true {
 			sum = sum.Add(sum, i)
