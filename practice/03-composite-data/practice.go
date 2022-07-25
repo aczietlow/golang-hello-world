a0e8343c4f05010db2db1cf188b9d5e1c6f514c2
aczietlow <aczietlow@gmail.com>
2022-07-25 08:32:11 -0400

    More slicing and dicing

--- practice/03-composite-data/practice.go
+++ practice/03-composite-data/practice.go
@@ -29,6 +29,34 @@ func main() {
 	// [8, 16, 32, 64, 128, 256]
 	// [4, 8, 16, 32, 64, 128]
 	three()
+
+	// Provided a given slice append to the slice the value of 52
+	// print out the slice
+	// in ONE statement append to that slice these values
+	// 53, 54, 55
+	// print out the slice
+	// append to the slice x:=[]int{56,57,58,59,60}
+	// print out the slice
+	four()
+
+	// To delete from a slice we use append() along with slicing.
+	// Start with slice x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	// use append and slicing to get the following values
+	// [42,43,44,48,49,50,51]
+	five()
+
+	// Create a slice to store the names of all the states in the US.
+	// Use make and append to do this.
+	// Goal: do not have the array that underlines the slice created more than once.
+	// What is the length of your slice?
+	// what is the capacity?
+	// Print out all the values, along with their index position, without using the range clause.
+	six()
+
+	//seven()
+	//eight()
+	//nine()
+	//ten()
 }
 
 func one() {
@@ -54,4 +82,38 @@ func three() {
 	fmt.Println(s[5:])
 	fmt.Println(s[2:8])
 	fmt.Println(s[1:7])
+}
+
+func four() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x = append(x, 52)
+	fmt.Println(x)
+
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+
+	y := []int{56, 57, 58, 59, 60}
+	x = append(x, y...)
+
+	fmt.Println(x)
+}
+
+func five() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	y := append(x[:3], x[6:]...)
+	fmt.Println(y)
+}
+
+func six() {
+	// This works, but I'm concerned about having the change the len of the slice over and over again.
+	x := make([]string, 0, 50)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	x = append(x, `Alabama`, `Alaska`, `Arizona`, `Arkansas`, `California`, `Colorado`, `Connecticut`, `Delaware`, `Florida`, `Georgia`, `Hawaii`, `Idaho`, `Illinois`, `Indiana`, `Iowa`, `Kansas`, `Kentucky`, `Louisiana`, `Maine`, `Maryland`, `Massachusetts`, `Michigan`, `Minnesota`, `Mississippi`, `Missouri`, `Montana`, `Nebraska`, `Nevada`, `New Hampshire`, `New Jersey`, `New Mexico`, `New York`, `North Carolina`, `North Dakota`, `Ohio`, `Oklahoma`, `Oregon`, `Pennsylvania`, `Rhode Island`, `South Carolina`, `South Dakota`, `Tennessee`, `Texas`, `Utah`, `Vermont`, `Virginia`, `Washington`, `West Virginia`, `Wisconsin`, `Wyoming`)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	for i := 0; i < len(x); i++ {
+		fmt.Println(i, x[i])
+	}
 }
